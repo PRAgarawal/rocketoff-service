1c43fb647fbd2676f393e6e8e590cf21279f11a6
Raj Agarawal <raj@ordermygear.com>
2020-02-28 13:50:18 -0600

    test transport

--- transport_test.go
+++ transport_test.go
@@ -2,6 +2,7 @@ package rocketoff
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -17,11 +18,38 @@ func TestMakeHTTPHandler(t *testing.T) {
 	assert.Implements(t, (*http.Handler)(nil), result)
 }
 
+func TestCommandDecoder(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		request := &http.Request{}
+		mockCD := &mockCommandDecoder{}
+		mockCD.On("DecodeCommand", request).Return(&chat.Command{}, nil)
+		decoder := makeSlashCommandRequestDecoder(mockCD)
+		req, err := decoder(context.TODO(), request)
+
+		assert.Nil(t, err)
+		assert.Equal(t, &commandRequest{}, req)
+	})
+
+	t.Run("decoder error", func(t *testing.T) {
+		request := &http.Request{}
+		mockCD := &mockCommandDecoder{}
+		mockCD.On("DecodeCommand", mock.Anything).Return(nil, fmt.Errorf("OMG WE'RE $5 INTO LUXURY TAX TERRITORY"))
+		decoder := makeSlashCommandRequestDecoder(mockCD)
+		req, err := decoder(context.TODO(), request)
+
+		assert.Nil(t, req)
+		if assert.Error(t, err) {
+			assert.Contains(t, err.Error(), "LUXURY TAX")
+		}
+	})
+}
+
 type mockCommandDecoder struct {
 	mock.Mock
 }
 
 func (m *mockCommandDecoder) DecodeCommand(_ context.Context, request *http.Request) (*chat.Command, error) {
 	args := m.Called(request)
-	return args.Get(0).(*chat.Command), args.Error(1)
+	command, _ := args.Get(0).(*chat.Command)
+	return command, args.Error(1)
 }
